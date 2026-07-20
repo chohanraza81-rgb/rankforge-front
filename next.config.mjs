a2fb573c0b9f634a974011596b5b5a89868625a4
chohanraza81-rgb <chohanraza81@gmail.com>
2026-07-21 01:19:41 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['rankforge-front.vercel.app'],
+  },
+  // For production
+  output: 'standalone',
 };
 
 export default nextConfig;
